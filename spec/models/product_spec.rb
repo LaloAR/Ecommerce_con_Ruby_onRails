d2cd79a12ad87b024a99cdc217d560cbdfe8f16a
Eduardo <eduardo.alcantara.rios@outlook.com>
2018-06-11 22:25:43 -0500

    Terminado

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id                  :integer          not null, primary key
+#  name                :string
+#  pricing             :integer
+#  description         :text
+#  user_id             :integer
+#  avatar_file_name    :string
+#  avatar_content_type :string
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  it{ should validate_presence_of :name }
+  it{ should validate_presence_of :user }
+  it{ should validate_presence_of :pricing }
+
+  it "sholud validate pricing > 0" do
+  	# create lo guarda en la base de datos
+  	# build lo guarda en memoria
+  	#product = FactoryGirl.create(:product)
+  	product = FactoryGirl.build(:product, pricing: 0)
+  	expect(product.valid?).to be_falsy
+  end
+end
